e0b4dc1b21147637b21393eb84356821b3ddc715
Violaine Soulas <vhsoulas@gmail.com>
2025-06-05 12:52:17 +0200

    Premier commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Banner from './Banner'
+import Cart from './Cart'
+
+function App() {
+  return <div><Banner /><Cart /></div>
+
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,4 @@
+function Banner () {
+    return <h1> La maison Jungle</h1>
+}
+export default Banner
