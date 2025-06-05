aa6b57a1edb07377b89b582f0ab12093cb3473ae
Violaine Soulas <vhsoulas@gmail.com>
2025-06-05 19:28:24 +0200

    Ajout d'une image à Banner + Cart.js + Cart.css

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,12 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
 function Banner () {
+    const title = 'La maison jungle'
     return (
         <div className='lmj-banner'>
-            <h1> La maison Jungle</h1> 
+            <img src = {logo} alt='La maison jungle' className='lmj-logo' />
+            <h1 className='lmj-title'>{title}</h1> 
         </div>
     );
 }
