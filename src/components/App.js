8aebbf727e46257c2d36849920514b2ac667b761
Violaine Soulas <vhsoulas@gmail.com>
2025-06-08 17:59:27 +0200

    P2C4 : done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import Banner from './Banner'
-import Cart from './Cart'
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 
 function App() {
-  return(
-    <div>
-      <Banner />
-      <Cart />
-      <ShoppingList />
-    </div>
-  )
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
-export default App;
+export default App
